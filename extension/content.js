2195a70711f204a0e32ee437058130b2591605ac
Vinícius Michelutti <viniciusmichelutti@gmail.com>
2020-07-14 14:30:59 -0300

    Pegando o elemento reto

--- extension/content.js
+++ extension/content.js
@@ -3,39 +3,24 @@
 
     // Opens a port to communicate messages
     var port = chrome.runtime.connect({name: "channel"});
+    var isValidHost = window.location.host.indexOf('app.mercos.com') !== -1
+    var selectorSlug = '#slug-para-extensao-chrome'
 
-    // Sends a message to background.js
-    // port.postMessage({});
-
-    var isMercos = window.location.host.indexOf('app.mercos.com') !== -1
-    var isMeusPedidos = window.location.host.indexOf('app.meuspedidos.com.br') !== -1
-    var isValidHost = isMercos || isMeusPedidos
-
-    // these selectors are quite fragile, it would be ideal to create more reliable ones.
-    var selector = isMercos
-        ? '#js-div-global > div.overlay > section > div.container-fluid > div > div > div.row-fluid > div.span9 > div > div.box-header > i:nth-child(2)'
-        : '#main h2.text-transparent small'
-
-    if (isValidHost
-        && window.location.pathname.indexOf('/admin/') !== -1
+    if (isValidHost && window.location.pathname.indexOf('/admin/') !== -1
         && !document.querySelector('ul.motivos')
         &&  document.getElementById('id_login')
         &&  document.getElementById('id_senha')
         &&  document.getElementById('form')) {
 
-        port.postMessage({pode_logar: true});
-    }
-    else if (isValidHost
-        && window.location.pathname.indexOf('/empresas/') !== -1
-        && document.querySelector(selector)) {
-
-        port.postMessage({tem_slug: true});
+        port.postMessage({ pode_logar: true });
+    } else if (isValidHost && window.location.pathname.indexOf('/empresas/') !== -1 && document.querySelector(selectorSlug)) {
+        port.postMessage({ tem_slug: true });
     }
 
     // Listens to messages from background.js
     port.onMessage.addListener(function (msg) {
         if (msg.send_me_slug) {
-            var element = document.querySelector(selector);
+            var element = document.querySelector(selectorSlug);
             if (element) {
                 window.console.log('sending slug');
                 var slug = element.innerHTML.trim();
